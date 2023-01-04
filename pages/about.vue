6735cb4da4febe3eb87facb59026cb2a9678a957
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-01-04 22:18:38 +0500

    Added Pages

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit alias et quam esse nisi ad saepe at natus aspernatur laborum.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+      h2{
+          margin-bottom: 20px;
+          font-size: 36px;
+      }
+      p{
+          margin: 20px 0px;
+      }
+  </style>
