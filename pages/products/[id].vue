812df59d47538b78729ceff3aa5d72a771d6f671
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-01-05 13:39:33 +0500

    Added Dynamic Routes

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product Details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, accusamus!</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
